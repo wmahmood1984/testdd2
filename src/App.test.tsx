71380716620bc448e0fb828b9ba3ec87bc74bddc
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-08-15 15:12:59 +0500

    added .travis file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello world/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello PIAIC/i);
   expect(linkElement).toBeInTheDocument();
 });
